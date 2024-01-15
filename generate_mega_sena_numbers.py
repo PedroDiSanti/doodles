ba570205f6c0e10dd1924f1c9fb0b68ac40b2ea2
PedroDiSanti <pedroa.disanti@gmail.com>
2024-01-15 20:32:14 -0300

    Refactor generate_mega_sena function and modify game logic
    The changes include the refactoring of the main function for better performance and readability. With the resolved bug, the generation of repeated numbers is no longer an issue. Additionally, the game logic is now improved to run multiple games in a more efficient manner by changing from using a while loop decrementing games count to a for loop.

--- generate_mega_sena_numbers.py
+++ generate_mega_sena_numbers.py
@@ -1,22 +1,31 @@
 import random
 
 
-def generate_mega_sena(quantity_of_numbers_to_generate=6, list_of_numbers=[]):
-    try:
-        for quantity in range(quantity_of_numbers_to_generate):
-            number_to_add = random.randint(1, 60)
-            if number_to_add in list_of_numbers:
-                number_to_add = generate_mega_sena(1)[0]
+def generate_mega_sena(quantity_of_numbers_to_generate=6, list_of_numbers=None):
+    """Generates a specified quantity of unique random numbers ranging from 1 to 60. 
+
+    Args:
+        quantity_of_numbers_to_generate (int): Quantity of unique random numbers to generate. Default is 6.
+        list_of_numbers (list): List to append the generated numbers to. If not specified,
+                                a new list will be created.
+
+    Returns:
+        list_of_numbers (list): List containing the generated unique random numbers.
+    """
+    if list_of_numbers is None:
+        list_of_numbers = []
+
+    while len(list_of_numbers) < quantity_of_numbers_to_generate:
+        number_to_add = random.randint(1, 60)
+        if number_to_add not in list_of_numbers:
             list_of_numbers.append(number_to_add)
-        list_of_numbers.sort()
-        return list_of_numbers
-    except Exception as error:
-        print(f"ERROR: {error}")
+
+    list_of_numbers.sort()
+    return list_of_numbers
 
 
 if __name__ == '__main__':
-    quantity_of_games = 5
-    while quantity_of_games > 0:
+    quantity_of_games = 2
+    for _ in range(quantity_of_games):
         numbers = generate_mega_sena(quantity_of_numbers_to_generate=6, list_of_numbers=[])
         print(numbers)
-        quantity_of_games -= 1
